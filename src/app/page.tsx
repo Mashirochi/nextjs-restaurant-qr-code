c7dde92a68f3c9fe29acb573618b3b2238f2062c
Mashirochi <dungne1132@gmail.com>
2025-12-13 16:05:48 +0700

    add github action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-wrap items-center gap-2 md:flex-row">
       <Button variant="outline">Button</Button>
+      quality over quantity
     </div>
   );
 }
