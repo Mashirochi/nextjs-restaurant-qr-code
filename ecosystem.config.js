6ce17788401e491c68af121cda1c60cb551eaa6d
Mashirochi <dungne1132@gmail.com>
2025-12-22 14:50:05 +0700

    minimize code

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,15 +2,7 @@ module.exports = {
   apps: [
     {
       name: "NextJs",
-      script: "node_modules/next/dist/bin/next",
-      args: "start -p 3000",
-      cwd: "/var/www/nextjs-restaurant-qr-code",
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      env: {
-        NODE_ENV: "production",
-      },
+      script: "npm run start",
     },
   ],
 };
